dd167a9846a4cb22bb7d2a75f372c33a76d4ab6c
Hosamaneprashanth <prashanth.18035@gmail.com>
2023-09-28 22:15:17 +0530

    local commit

--- src/test/java/Gitbashpractice/Gitbash.java
+++ src/test/java/Gitbashpractice/Gitbash.java
@@ -4,7 +4,7 @@ public class Gitbash {
         public static void main(String[] args) {
 			System.out.println("prashanth");
 		System.out.println("hi prashanth");
-		oiwicnuienuiern;
+		System.out.println("fhjherbjhe");
 		}
 
 }
