ab86f245056eb994b6507b6276193de6f2e08a27
Hosamaneprashanth <143492537+Hosamaneprashanth@users.noreply.github.com>
2023-09-28 22:23:28 +0530

    Update Gitbash.java

--- src/test/java/Gitbashpractice/Gitbash.java
+++ src/test/java/Gitbashpractice/Gitbash.java
@@ -3,8 +3,6 @@ package Gitbashpractice;
 public class Gitbash {
         public static void main(String[] args) {
 			System.out.println("prashanth");
-		System.out.println("hi prashanth");
-		oiwicnuienuiern;
 		}
 
 }
